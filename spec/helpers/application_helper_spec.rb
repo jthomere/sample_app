d696413ccb5a8cd8c1d9c901736a25477c6b3354
Jerome Thomere <jerome@thomere.org>
2013-01-18 15:01:28 -0800

    Finish layout and routes

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -10,7 +10,7 @@ describe ApplicationHelper do
       full_title('foo').should  =~ /^Ruby on Rails Tutorial Sample/
     end
     it "should not include a bar on the home page" do
-      full_title(''),should_not =~ /\|/
+      full_title('').should_not =~ /\|/
     end
   end
 end
